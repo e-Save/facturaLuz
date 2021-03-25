789eb459f0755e1f4d682e9f8eca67dbbc767764
Omar Alarcon <omar98alarcon@gmail.com>
2021-03-25 14:37:02 +0100

    Partial local - Omar - 25/03/2021

--- js/nuestros-servicios.js
+++ js/nuestros-servicios.js
@@ -1,5 +1,5 @@
 /* load header & footer */
 $(document).ready(function() {
-    $('.nuestros-servicios').load("https://partials-e-save.s3.eu-central-1.amazonaws.com/partial/nuestros_servicios.html");
+    $('.nuestros-servicios').load("partials/nuestros_servicios.html");
 });
   
