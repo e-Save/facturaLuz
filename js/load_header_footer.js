6367f92f19f867be92678097097cd1fa7268ecb8
Omar Alarcon <omar98alarcon@gmail.com>
2021-03-25 14:03:17 +0100

    Prueba partial- Omar - 25/03/2021

--- js/load_header_footer.js
+++ js/load_header_footer.js
@@ -1,5 +1,7 @@
 /* load header & footer */
+
 $(document).ready(function() {
-    $('.nuestros-servicios').load("https://partials-e-save.s3.eu-central-1.amazonaws.com/partial/nuestros_servicios.html");
+    $('.header').load("partials/header.html");
+    $('.footer').load("partials/footer.html");
 });
   
